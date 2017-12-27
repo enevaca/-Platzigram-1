d03371faeedc462904345ef1bd1749b4087f86b6
Noel Vaca Moreno <noel.vm.2501@gmail.com>
2017-12-26 22:04:58 -0400

    Actualización de lib

---
+++ app/src/main/java/com/platzi/platzigram/MyFirebaseMessagingService.java
@@ -0,0 +1,17 @@
+package com.platzi.platzigram;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by enevaca on 26/12/2017.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.e("FIREBASE", remoteMessage.getNotification().getBody());
+    }
+}
